4091277ec4ed60c42079101c34a6ac52980aab78
Juan Herrera <juannicolash01@gmail.com>
2021-07-25 19:32:06 -0300

    add base de datos, fixbugs

--- bd.sql
+++ bd.sql
@@ -1,4 +1,13 @@
 use blog_cac2124;
+DROP TABLE IF EXISTS articulo;
+DROP TABLE IF EXISTS autor;
+DROP TABLE IF EXISTS contacto;
+DROP TABLE IF EXISTS administrador;
+DROP TABLE IF EXISTS categoria;
+
+CREATE TABLE administrador (
+id_administrador INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+tipo VARCHAR(255) NOT NULL);
 
 CREATE TABLE categoria (
 id_categoria INT(255) AUTO_INCREMENT NOT NULL PRIMARY KEY,
@@ -10,15 +19,16 @@ nombre  VARCHAR(255) NOT NULL,
 apellido VARCHAR(255) NOT NULL,
 correo VARCHAR(255) NOT NULL UNIQUE,
 password VARCHAR(255) NOT NULL,
-id_administrador BOOL NOT NULL DEFAULT 0);
+id_administrador INT(255) NOT NULL DEFAULT 0,
+FOREIGN KEY(id_administrador) REFERENCES administrador(id_administrador));
 
 CREATE TABLE contacto (
-id_mensaje INTEGER(255) AUTO_INCREMENT NOT NULL PRIMARY KEY,
+id_mensaje INT(255) AUTO_INCREMENT NOT NULL PRIMARY KEY,
 email VARCHAR(255) NOT NULL,
+asunto VARCHAR(255) NOT NULL,
 mensaje VARCHAR(1000) NOT NULL,
-id_administrador BOOL NOT NULL DEFAULT 1,
-id_autor INT(255) NOT NULL,
-FOREIGN KEY(id_autor) REFERENCES autor(id_autor));
+id_administrador INT(255) NOT NULL DEFAULT 1,
+FOREIGN KEY(id_administrador) REFERENCES administrador(id_administrador));
 
 CREATE TABLE articulo (
 id_articulo INT(255) AUTO_INCREMENT NOT NULL PRIMARY KEY,
